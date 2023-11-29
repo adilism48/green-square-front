5b30ea97407b66026d24d7c3c34096c9a7c7c42d
aizentel <aizentel>
2023-11-30 00:54:55 +0600

    promo added

---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,28 @@
+import './promo.css'
+import promoImg from '../../img/photo.png'
+
+const Promo = () => {
+    return (
+        <section className="promo">
+            <div className="container">
+                <div className="promo__content">
+                    <div className="promo__text">
+                        <div className="promo__title">
+                            Зелёный Квадрат это
+                            онлайн каталог
+                            овощей и фруктов
+                        </div>
+                        <div className="promo__small-title">
+                            Которые вы можете заказать у нас
+                        </div>
+                    </div>
+                    <div className="promo__img">
+                        <img src={promoImg} alt="promoImg"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Promo;
